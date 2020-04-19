ccbd730bccbd96d7409bb40a07bb887fed9b9214
andres <andres@localhost.localdomain>
2020-04-19 15:15:29 -0600

    Acceso a paginas success

--- Clientes/urls.py
+++ Clientes/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
 
-    path('clientes/', views.clientes_list, name='clientes_list'),
-    path('clientes/new/', views.clientes_new, name='clientes_new'),
-    path('suscripcion/new/', views.suscripcion_new, name='suscripcion_new'),
-    path('suscripciones/', views.suscripciones, name='suscripciones'),
-    path('descuento/new/', views.descuento_new, name='descuento_new'),
-    path('descuentos/', views.descuentos, name='descuentos'),
+    path('clientes/', login_required(views.clientes_list), name='clientes_list'),
+    path('clientes/new/', login_required(views.clientes_new), name='clientes_new'),
+    path('suscripcion/new/', login_required(views.suscripcion_new), name='suscripcion_new'),
+    path('suscripciones/', login_required(views.suscripciones), name='suscripciones'),
+    path('descuento/new/', login_required(views.descuento_new), name='descuento_new'),
+    path('descuentos/', login_required(views.descuentos), name='descuentos'),
     
 ]
--- Facturacion/urls.py
+++ Facturacion/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
 
-    path('facturas/', views.facturas_list, name='facturas_list'),
-    path('facturas/nueva', views.factura_new, name='factura_new'),    
-    path('facturas/detalle', views.factura_detalle, name='factura_detalle'),
-    path('facturas/detail', views.factura_detail, name='factura_detail'),
+    path('facturas/', login_required(views.facturas_list), name='facturas_list'),
+    path('facturas/nueva', login_required(views.factura_new), name='factura_new'),    
+    path('facturas/detalle', login_required(views.factura_detalle), name='factura_detalle'),
+    path('facturas/detail', login_required(views.factura_detail), name='factura_detail'),
 ]
